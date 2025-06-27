570d787711630e74f0bcf6c8e8f4f17bc59fbd69
Rafael305 <fraq86@gmail.com>
2025-06-27 18:29:27 -0300

    primer commit

---
+++ Market/market.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#@app.route("/") # Esto es un decorador
+#def hello_world():
+#    return "<h1>Hello world</h1>"
+
+@app.route("/about/<username>") 
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
+
+@app.route("/home-page") # Se le puede poner dos rutas a un mismo método
+@app.route("/")
+def home_page():
+    return render_template("home.html")
