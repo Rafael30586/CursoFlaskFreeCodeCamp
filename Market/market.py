3c2984063ccf759f5749f8391b6106169224ef3b
Rafael305 <fraq86@gmail.com>
2025-06-30 17:49:10 -0300

    bucle for en tabla de html

--- Market/market.py
+++ Market/market.py
@@ -13,3 +13,14 @@ def about_page(username):
 @app.route("/")
 def home_page():
     return render_template("home.html")
+
+@app.route("/market")
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': 5986040, 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': 9583040, 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': 59201940, 'price': 150}
+    ]
+    
+    
+    return render_template("market.html", items=items)
