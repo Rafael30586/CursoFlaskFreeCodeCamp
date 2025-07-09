027de4e1459b996280d78a2d97010a8ab366409a
Rafael305 <fraq86@gmail.com>
2025-07-09 18:47:53 -0300

    validacion de datos en formularios

--- Market/market/routes.py
+++ Market/market/routes.py
@@ -1,7 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, redirect, url_for
 from market import app # No se importa desde __init__ porque, aparentemente, también se puede importar desde un paquete, en este caso, market
-from market.models import item
+from market.models import item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route("/about/<username>") 
 def about_page(username):
@@ -18,7 +19,18 @@ def market_page():
     
     return render_template("market.html", items=items)
 
-@app.route("/register")
+@app.route("/register" ,methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        with app.app_context():
+            db.session.add(user_to_create)
+            db.session.commit()
+            return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
     return render_template('register.html',form=form)
